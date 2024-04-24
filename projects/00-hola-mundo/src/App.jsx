3839a46e55665c6e29d803efe83115e7ae0b53c5
lemus777 <lemus777@gmail.com>
2024-04-24 14:12:40 +0200

    first commit hola mundo

---
+++ projects/00-hola-mundo/src/App.jsx
@@ -0,0 +1,17 @@
+export function App () {
+  return (
+  <article>
+    <header>
+      <img alt='El avatar de midudev' src='https://unavatar.io/midudev' />
+      <div>
+        <strong>Miguel Ángel Durán</strong>
+        <span>@midudev</span>
+      </div>
+    </header>
+
+    <aside>
+      <button>Seguir</button>
+    </aside>
+  </article>
+  )
+}
