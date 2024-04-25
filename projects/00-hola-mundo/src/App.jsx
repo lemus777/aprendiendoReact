11cd261157699677276baf4ef2cfd1df91cac7c0
lemus777 <lemus777@gmail.com>
2024-04-25 13:22:58 +0200

    first project finished

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -1,17 +1,34 @@
+import './App.css'
+import { TwitterFollowCard } from './TwitterFollowCard'
+
+const users = [
+  {
+    userName: 'midudev',
+    name: 'Miguel Ángel Duran',
+    isFollowing: true
+  },
+  {
+    userName: 'pheralb',
+    name: 'Pablo Heraldo',
+    isFollowing: false
+  },
+  {
+    userName: 'vxnder',
+    name: 'Vanderhart',
+    isFollowing: false
+  }
+]
+
 export function App () {
   return (
-  <article>
-    <header>
-      <img alt='El avatar de midudev' src='https://unavatar.io/midudev' />
-      <div>
-        <strong>Miguel Ángel Durán</strong>
-        <span>@midudev</span>
-      </div>
-    </header>
-
-    <aside>
-      <button>Seguir</button>
-    </aside>
-  </article>
+    <div className='App'>
+      {
+        users.map(({ userName, name, isFollowing }) => (
+            <TwitterFollowCard key={userName} userName={userName} initialIsFollowing={isFollowing}>
+              {name}
+            </TwitterFollowCard>
+        ))
+      }
+    </div>
   )
 }
