ddcd17ece3963044fd645ad141af0451dbfd67ba
lemus777 <lemus777@gmail.com>
2024-04-26 13:42:07 +0200

    tic-tac-toe finished

---
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -0,0 +1,86 @@
+import { useState } from 'react'
+import confetti from 'canvas-confetti'
+
+import { Square } from './components/Square'
+
+import { TURNS } from './constants'
+import { checkWinnerFrom } from './logic/board'
+import { checkEndGame } from './logic/board'
+import { WinnerModal } from './components/WinnerModal'
+
+function App() {
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem('board')
+    return boardFromStorage ? JSON.parse(boardFromStorage) : Array(9).fill(null)
+  })
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem('turn')
+    return turnFromStorage ?? TURNS.X 
+  })
+  // winner null es que no hay ganador, false es que hay empate
+  const [winner, setWinner] = useState(null) 
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null))
+    setTurn(TURNS.X)
+    setWinner(null)
+
+    window.localStorage.removeItem('board')
+    window.localStorage.removeItem('turn')
+  }
+
+  const updateBoard = (index) => {
+    // si board en ese index tiene algo o hay un ganador
+    // no hace nada
+    if (board[index] || winner) return
+    // si sigue siendo null board en ese index actualiza el tablero
+    const newBoard = [...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
+    // cambiar el turno
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+    // guardar aquí partida
+    window.localStorage.setItem('board', JSON.stringify(newBoard))
+    window.localStorage.setItem('turn', newTurn)
+    // revisar si hay ganador
+    const newWinner = checkWinnerFrom(newBoard)
+    if (newWinner) {
+      confetti()
+      setWinner(newWinner)
+    } else if (checkEndGame(newBoard)) {
+      setWinner(false) // empate
+    }
+  }
+
+  return (
+    <main className="board">
+      <h1>Tic tac toe</h1>
+      <button onClick={resetGame}>Reset del juego</button>
+      <section className="game">
+        {
+          board.map((square, index) => {
+            return (
+              <Square
+                key={index}
+                index={index}
+                updateBoard={updateBoard}
+              >
+                {square}
+              </Square>
+            )
+          })
+        }
+      </section>
+
+      <section className='turn'>
+        <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
+        <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
+      </section>
+
+      <WinnerModal winner={winner} resetGame={resetGame} />
+    </main>
+  )
+}
+
+export default App
---
+++ projects/02-tic-tac-toe/src/components/Square.jsx
@@ -0,0 +1,13 @@
+export const Square = ({ children, isSelected, updateBoard, index }) => {
+  const className = `square ${isSelected ? 'is-selected' : ''}`
+
+  const handleclick = () => {
+    updateBoard(index)
+  }
+
+  return (
+    <div onClick={handleclick} className={className}>
+      {children}
+    </div>
+  )
+}
